029d23cb3d5e28283d9e3e25fcd06862251e7127
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-10 14:21:48 +0300

    login ekranı ui oluşturuldu

--- login_screen/lib/main.dart
+++ login_screen/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -32,13 +33,84 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranYuksekligi =  ekranBilgisi.size.height;
+    final double ekranGenisligi = ekranBilgisi.size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.indigoAccent,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: ekranYuksekligi/20),
+                child: SizedBox(
+                  width: ekranGenisligi/4,
+                  height: ekranYuksekligi/6,
+                  child: Image.asset('assets/key.png'),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(ekranYuksekligi/30),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Kullanıcı Adı",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(ekranYuksekligi/30),
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Şifre",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(ekranYuksekligi/30),
+                child: SizedBox(
+                  width: ekranGenisligi/2,
+                  height: ekranYuksekligi/15,
+                  child: ElevatedButton(
+                    onPressed: (() {
+                      print("Giriş yapıldı..");
+                    }),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange
+                    ),
+                    child: Text("Giriş Yap", style: TextStyle(fontSize: ekranGenisligi/25),),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print("Yardıma ihtiyacın yok sen her şeyi yaparsın");
+                },
+                child: const Text(
+                  "Help",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontWeight: FontWeight.bold,
+                    //fontSize: ekranGenisligi/30,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
